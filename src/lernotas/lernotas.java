6b0f9406756d691bfee66d5b6f1ad3854d2cb0d0
João Emanuel <joaoemanueldanielski@gmail.com>
2023-08-17 17:06:23 -0300

    primeira

---
+++ src/lernotas/lernotas.java
@@ -0,0 +1,21 @@
+package lernotas;
+
+import java.util.Scanner;
+
+public class lernotas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double nota=0;
+		for(int i = 0 ; i<6;i++) {
+			System.out.println("digite a nota de um aluno");
+			double notas = sc.nextDouble();
+
+			
+		}
+		
+		
+	}
+
+}
