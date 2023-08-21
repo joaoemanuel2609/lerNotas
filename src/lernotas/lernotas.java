45e69dde523094b0d87057da46d49ebb37879bfd
João Emanuel <joaoemanueldanielski@gmail.com>
2023-08-21 14:28:54 -0300

    segunda modificaçao , adicionando funçao de media de notas

--- src/lernotas/lernotas.java
+++ src/lernotas/lernotas.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class lernotas {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		double nota=0;
-		for(int i = 0 ; i<6;i++) {
-			System.out.println("digite a nota de um aluno");
-			double notas = sc.nextDouble();
 
-			
+		Scanner sc = new Scanner(System.in);
+		double[] nota = new double[5];
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("digite a nota de um aluno");
+			nota[i] = sc.nextDouble();
+
 		}
+
+		double soma = 0;
+		for (double resultado : nota) {
+
+			soma += resultado;
 		
-		
+		}
+		System.out.println(soma/nota.length);
 	}
 
 }
