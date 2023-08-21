fb0acf9f39a07581550848ecc5179ba4435c4ce0
João Emanuel <joaoemanueldanielski@gmail.com>
2023-08-21 15:12:47 -0300

    ultima atualizaçao, menos e maiores notas

--- src/lernotas/lernotas.java
+++ src/lernotas/lernotas.java
@@ -1,5 +1,6 @@
 package lernotas;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class lernotas {
@@ -17,6 +18,8 @@ public class lernotas {
 		double nb =0;
 		double nt=0;
 		double soma = 0;
+		double notaantiga=0;
+		double maiornota = 0;
 		for (double resultado : nota) {
 			
 			if(resultado<6) {
@@ -25,11 +28,18 @@ public class lernotas {
 			nt = nt+1;
 			}
 			soma += resultado;
+			if (notaantiga<resultado) {
+				maiornota = resultado;
+			}
+				 notaantiga = resultado;
 		}
+		
 		System.out.println("media das notas "+soma/nota.length);
 		System.out.println("notas abaixo de 6 foram "+ nb);
 		System.out.println("notas acima de 6 foram "+nt);
-		
+		System.out.println("maior nota "+ maiornota);
+		Arrays.sort(nota);
+		System.out.println("menor nota "+ nota[0]);
 	}
 
 }
